cadc68648e7ab79ab74a045e11185576f40f04ac
Dylan Buchi <devybuchi@gmail.com>
2020-09-25 10:25:04 -0300

    add functions: follow_original_author, like_python_tweets and get_api in main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import dotenv
-import tweepy
 import os
-from dotenv import load_dotenv
+import tweepy
+import dotenv
 
-if __name__ == "__main__":
-    load_dotenv()
+
+def get_api():
+    """
+    Load api keys in .env file, basic auth for twitter API and return the API
+    """
+
+    dotenv.load_dotenv()
 
     consumer_key = os.environ.get('consumer_key')
     consumer_secret = os.environ.get('consumer_secret')
@@ -16,8 +20,52 @@ if __name__ == "__main__":
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
+    return api
 
-    public_tweets = api.home_timeline()
 
-    user = api.me()
-    print(user)
+def follow_original_author(api, subject, number_of_authors):
+    """follow original authors by filtering retweets using tweets subject 
+
+    Args:
+        api (API): Twitter API
+        subject (str): pick a subject for the tweets to like ex: 'python'
+        number_of_authors (int): how many tweets to like
+    """
+    filter_retweets = "-filter:retweets"
+
+    for follower in tweepy.Cursor(
+            api.search,
+            f"{subject} {filter_retweets}").items(number_of_authors):
+        try:
+            api.create_friendship(screen_name=follower.author.screen_name)
+            print(follower)
+        except tweepy.TweepError as error:
+            print(error)
+        except StopIteration:
+            break
+    print("done")
+
+
+def like_python_tweets(api, subject, number_of_tweets):
+    """like tweets by subject
+
+    Args:
+        api (API): Twitter API
+        subject (str): pick a subject for the tweets to like ex: 'python'
+        number_of_tweets (int): how many tweets to like
+    """
+    for tweet in tweepy.Cursor(api.search, subject).items(number_of_tweets):
+
+        try:
+            tweet.favorite()
+            print('<3')
+        except tweepy.TweepError as error:
+            print(error)
+        except StopIteration:
+            break
+    print("done")
+
+
+if __name__ == "__main__":
+
+    api = get_api()
